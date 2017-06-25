cb444bc0212c683ebcadd5b84c7411a7675626c8
Harris Brakmic <brakmic@gmail.com>
2017-06-25 20:58:23 +0200

    stricter checks in BaseStoreToken contract

--- contracts/Tokens/BaseStoreToken.sol
+++ contracts/Tokens/BaseStoreToken.sol
@@ -17,14 +17,25 @@ contract BaseStoreToken is ERC20, SafeMath {
 	  uint8 public decimals;
 
     /**
-      * @dev Fix for the ERC20 short address attack.
-      */
-     modifier onlyPayloadSize(uint256 size) {
+        @notice Fix for the ERC20 short address attack.
+        @param size data length
+    */
+    modifier onlyPayloadSize(uint256 size) {
         if(msg.data.length < size + 4) {
           throw;
         }
         _;
-     }
+    }
+
+    /**
+        @notice accept only valid addresses
+        @param _address Memory address
+    */
+    modifier onlyValidAddress(address _address) {
+        require(_address != 0x0);
+        _;
+    }
+
     /**
           @notice Default constructor
     */
@@ -41,7 +52,9 @@ contract BaseStoreToken is ERC20, SafeMath {
           @param _to Address to transfer token to
           @param _value Value to be transferred
     */
-	  function transfer(address _to, uint256 _value) onlyPayloadSize(2 * 32)
+	  function transfer(address _to, uint256 _value)
+                                                  onlyPayloadSize(2 * 32)
+                                                  onlyValidAddress(_to)
                                                                 returns (bool) {
 	    balances[msg.sender] = safeSub(balances[msg.sender], _value);
 	    balances[_to] = safeAdd(balances[_to], _value);
@@ -55,6 +68,8 @@ contract BaseStoreToken is ERC20, SafeMath {
           @param _value Amount of tokens to be transferred
     */
   	function transferFrom(address _from, address _to, uint256 _value)
+                                                      onlyValidAddress(_from)
+                                                      onlyValidAddress(_to)
                                                                 returns (bool) {
 	    var _allowance = allowed[_from][msg.sender];
 	    balances[_to] = safeAdd(balances[_to], _value);
@@ -69,7 +84,9 @@ contract BaseStoreToken is ERC20, SafeMath {
           @param _spender Spender of the funds
           @param _value Amounf of tokens to be spent
     */
-  	function approve(address _spender, uint256 _value) returns (bool) {
+  	function approve(address _spender, uint256 _value)
+                                                    onlyValidAddress(_spender)
+                                                                returns (bool) {
     	allowed[msg.sender][_spender] = _value;
     	Approval(msg.sender, _spender, _value);
     	return true;
@@ -79,7 +96,8 @@ contract BaseStoreToken is ERC20, SafeMath {
           @param _address Address whose balance should be queried
           @return uint256 Represents the balance
     */
-  	function balanceOf(address _address) constant returns (uint256 balance) {
+  	function balanceOf(address _address) onlyValidAddress(_address)
+                                            constant returns (uint256 balance) {
   		return balances[_address];
   	}
     /**
@@ -88,7 +106,10 @@ contract BaseStoreToken is ERC20, SafeMath {
           @param _spender Spender's address
           @return uint256 Amount of tokens available to the spender
     */
-  	function allowance(address _owner, address _spender) constant
+  	function allowance(address _owner, address _spender)
+                                                   onlyValidAddress(_owner)
+                                                   onlyValidAddress(_spender)
+                                                   constant
                                                    returns (uint256 remaining) {
     	return allowed[_owner][_spender];
   	}
